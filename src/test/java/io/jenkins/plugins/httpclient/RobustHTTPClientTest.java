89332df947cbd14128aad9fd1fcc3730d05e2214
Mark Waite <mark.earl.waite@gmail.com>
2023-09-10 06:54:09 -0600

    Add a RobustHTTPClient downloadFile test
    Also revises the existing test to use example.com instead of x.com since
    there is now a site with the x.com domain name.
    
    Switched to hamcrest assertions because I'm more comfortable with
    their output.

--- src/test/java/io/jenkins/plugins/httpclient/RobustHTTPClientTest.java
+++ src/test/java/io/jenkins/plugins/httpclient/RobustHTTPClientTest.java
@@ -24,19 +24,50 @@
 
 package io.jenkins.plugins.httpclient;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
+import hudson.model.TaskListener;
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.WithoutJenkins;
 
 public class RobustHTTPClientTest {
 
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     @Test
+    @WithoutJenkins
     public void sanitize() throws Exception {
-        assertEquals("http://x.com/some/path", RobustHTTPClient.sanitize(new URL("http://x.com/some/path")));
-        assertEquals(
-                "https://x.com/some/path?…", RobustHTTPClient.sanitize(new URL("https://x.com/some/path?auth=s3cr3t")));
-        assertEquals(
-                "https://…@x.com/otherpath", RobustHTTPClient.sanitize(new URL("https://user:s3cr3t@x.com/otherpath")));
+        assertThat(
+                RobustHTTPClient.sanitize(new URL("http://example.com/some/long/path")),
+                is("http://example.com/some/long/path"));
+        assertThat(
+                RobustHTTPClient.sanitize(new URL("https://example.com/some/path?auth=s3cr3t")),
+                is("https://example.com/some/path?…"));
+        assertThat(
+                RobustHTTPClient.sanitize(new URL("https://user:s3cr3t@example.com/otherpath")),
+                is("https://…@example.com/otherpath"));
+    }
+
+    @Test
+    public void testDownloadFile() throws Exception {
+        JenkinsRule.WebClient wc = j.createWebClient();
+        RobustHTTPClient client = wc.executeOnServer(() -> {
+            return new RobustHTTPClient();
+        });
+        File f = new File(tempFolder.newFolder(), "jenkins-index.html");
+        client.downloadFile(f, j.getURL(), TaskListener.NULL);
+        assertThat(Files.readString(f.toPath()), containsString("<title>Dashboard [Jenkins]</title>"));
     }
 }
